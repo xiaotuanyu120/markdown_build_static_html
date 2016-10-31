2b63635e3a651dd69f596355e7f6caff4a244d91
zack <zhaopeiwu@outlook.com>
2016-10-31 03:53:26 +0000

    add post folder, delete cms.py

--- MyBlog/__init__.py
+++ MyBlog/__init__.py
@@ -1,5 +1,4 @@
 from flask import Flask, render_template
-from cms import content
 from topics import topics
 
 TOPIC_DICT = topics()
@@ -9,8 +8,3 @@ app = Flask(__name__)
 @app.route('/')
 def homepage():
     return render_template("index.html", TOPIC_DICT = TOPIC_DICT)
-
-
-@app.route('/linux_basic')
-def linux_basic():
-    return render_template("linux_basic.html")
